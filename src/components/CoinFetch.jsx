4524c2ba8e569a9056e5f1300f68ad79f7597118
roconn <roconn25@gmail.com>
2023-04-13 21:08:36 +0100

    styling updates + formatting

--- src/components/CoinFetch.jsx
+++ src/components/CoinFetch.jsx
@@ -1,122 +1,112 @@
 import React from "react";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
-import { TbReload } from 'react-icons/tb';
+import { TbReload } from "react-icons/tb";
 
-function CoinFetch (props) {
-    let [coinPrice, setPrice] = useState('loading..')
+function CoinFetch(props) {
+  let [coinPrice, setPrice] = useState("loading..");
 
-    let coinValue = props.coinValue
-    let currency = props.currency
+  let coinValue = props.coinValue;
+  let currency = props.currency;
 
-    
-    let [currencyData, setCurrencyData] = useState(currency)
-    let [coinData, setCoinData] = useState(coinValue)
-    
-    let [symbol, setSymbol] = useState('')
+  let [currencyData, setCurrencyData] = useState(currency);
+  let [coinData, setCoinData] = useState(coinValue);
 
-    // second useeffect to remove selection lag
-    useEffect(() => {
-    setCurrencyData(currency)
-    setCoinData(coinValue)
-    console.log(currency, coinValue)
-    })
-    
+  let [symbol, setSymbol] = useState("");
 
-    console.log(currency, coinValue)
-    console.log(currencyData, coinData)
+  // second useeffect to remove selection lag
+  useEffect(() => {
+    setCurrencyData(currency);
+    setCoinData(coinValue);
+    console.log(currency, coinValue);
+  });
 
-    
+  console.log(currency, coinValue);
+  console.log(currencyData, coinData);
 
-    useEffect(() => {
-       
-        // setCurrencyData(currency)
-        // setCoinData(coinValue)
+  useEffect(() => {
+    // setCurrencyData(currency)
+    // setCoinData(coinValue)
 
-        console.log('Set to fetch: ' + coinData + currencyData)
+    console.log("Set to fetch: " + coinData + currencyData);
 
-        //! Uncomment to ensure API fetch
-        if (coinData === 'Etherium') {
-            ethFetch(currencyData)
+    //! Uncomment to ensure API fetch
+    // ! This works to bring back the previous search but does not display what the previous search was
+    // attempt at dynamically fetching the data based on the checked radio boxes
+    // the state always loaded the previous input and I have not as yet fixed this bug
+    if (coinData === "Etherium") {
+      ethFetch(currencyData);
+    } else {
+      btcFetch(currencyData);
+    }
+  }, [props]);
+
+  // if eth
+  function ethFetch(currencyData) {
+    console.log("You are in ethFetch " + currencyData);
+
+    fetch("https://api.coinpaprika.com/v1/tickers/eth-ethereum?quotes=GBP,USD")
+      .then((response) => response.json())
+      .then((data) => {
+        if (currencyData === "GBP") {
+          console.log(data.quotes.GBP.price);
+          setPrice(data.quotes.GBP.price);
+          setSymbol("£");
+          // setCurrencyData('GBP')
         } else {
-            btcFetch(currencyData)
+          console.log(data.quotes.USD.price);
+          setPrice(data.quotes.USD.price);
+          setSymbol("$");
+          // setCurrencyData('USD')
         }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
 
-    }, [props])
+  // if btc
+  function btcFetch() {
+    console.log("You are in btcFetch " + currencyData);
 
-
-    
-
-
-    // if eth
-    function ethFetch(currencyData) {
-
-        console.log('You are in ethFetch ' + currencyData)
-
-        fetch('https://api.coinpaprika.com/v1/tickers/eth-ethereum?quotes=GBP,USD')
-        .then((response) => response.json())
-        .then((data) => {
-            if (currencyData === 'GBP') {
-                console.log(data.quotes.GBP.price)
-                setPrice(data.quotes.GBP.price)
-                setSymbol('£')
-                // setCurrencyData('GBP')
-            } else {
-                console.log(data.quotes.USD.price)
-                setPrice(data.quotes.USD.price)
-                setSymbol('$')
-                // setCurrencyData('USD')
-            }
-        })
-        .catch((err) => {
-            console.log(err.message)
-        })
-        
-    }
-
-    // if btc
-    function btcFetch() {
-
-        console.log('You are in btcFetch ' + currencyData)
-
-        fetch('https://api.coinpaprika.com/v1/tickers/btc-bitcoin?quotes=GBP,USD')
-        .then((response) => response.json())
-        .then((data) => {
-            if (currencyData === 'GBP') {
-                console.log(data.quotes.GBP.price)
-                setPrice(data.quotes.GBP.price)
-                setSymbol('£')
-                // setCurrencyData('GBP')
-            } else {
-                console.log(data.quotes.USD.price)
-                setPrice(data.quotes.USD.price)
-                setSymbol('$')
-            }
-        })
-        .catch((err) => {
-            console.log(err.message)
-        })
-        
-    }
-
-    
-    const handleClick = () => {
-        if (coinData === 'Etherium') {
-            ethFetch(currencyData)
+    fetch("https://api.coinpaprika.com/v1/tickers/btc-bitcoin?quotes=GBP,USD")
+      .then((response) => response.json())
+      .then((data) => {
+        if (currencyData === "GBP") {
+          console.log(data.quotes.GBP.price);
+          setPrice(data.quotes.GBP.price);
+          setSymbol("£");
+          // setCurrencyData('GBP')
         } else {
-            btcFetch(currencyData)
+          console.log(data.quotes.USD.price);
+          setPrice(data.quotes.USD.price);
+          setSymbol("$");
         }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  // fetch/refresh
+  const handleClick = () => {
+    if (coinData === "Etherium") {
+      ethFetch(currencyData);
+    } else {
+      btcFetch(currencyData);
     }
+  };
 
-    
-    
-
-    return (
-        <div>
-            <Button onClick={handleClick}>Fetch <TbReload /></Button>
-            Price: {symbol} {parseFloat(coinPrice).toFixed(2)}
-        </div>
-    )
+  return (
+    <div className="fetch">
+      <Button className="btn-fetch" onClick={handleClick}>
+        Fetch <TbReload />
+      </Button>
+      <h4>
+         {symbol}{parseFloat(coinPrice).toFixed(2)}
+      </h4>
+    </div>
+  );
 }
 
-export default CoinFetch
+export default CoinFetch;
--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,59 +1,65 @@
-import { useState } from 'react';
-import ToggleButton from 'react-bootstrap/ToggleButton';
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
-import CoinFetch from './CoinFetch'
+import { useState } from "react";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import CoinFetch from "./CoinFetch";
 
 function Toggle() {
+  let [coinValue, setValue] = useState(localStorage.getItem("coin"));
+  let [currency, setCurrency] = useState(localStorage.getItem("currency"));
+  //    let [currentCoin, setCoin] = useState('')
+  //    let [currentCurrency, setCurrentCurrency] = useState('')
 
-   let [coinValue, setValue] = useState(localStorage.getItem('coin'))
-   let [currency, setCurrency] = useState(localStorage.getItem('currency'))
-//    let [currentCoin, setCoin] = useState('')
-//    let [currentCurrency, setCurrentCurrency] = useState('')
-
-   const handleChangeCoin = (value) => {
-    setValue(value)
+  const handleChangeCoin = (value) => {
+    setValue(value);
     // setCoin(value)
-   }
+  };
 
-   const handleChangeCurrency = (value) => {
-    setCurrency(value)
+  const handleChangeCurrency = (value) => {
+    setCurrency(value);
     // setCurrentCurrency(value)
-   }
+  };
 
-   console.log(coinValue)
-   console.log(currency)
-   localStorage.clear()
-//    setting local storage
-   localStorage.setItem('coin', coinValue)
-   localStorage.setItem('currency', currency)
+  console.log(coinValue);
+  console.log(currency);
+  localStorage.clear();
+  //    setting local storage
+  localStorage.setItem("coin", coinValue);
+  localStorage.setItem("currency", currency);
 
   return (
     <>
-      <ToggleButtonGroup type="radio" name="coin-options" id="coin-select" defaultValue={'Etherium'} onChange={handleChangeCoin}>
-        <ToggleButton id="coin-radio-1" value={'Etherium'}>
+      <ToggleButtonGroup
+        type="radio"
+        name="coin-options"
+        id="coin-select"
+        defaultValue={"Etherium"}
+        onChange={handleChangeCoin}
+      >
+        <ToggleButton id="coin-radio-1" value={"Etherium"}>
           Etherium
         </ToggleButton>
-        <ToggleButton id="coin-radio-2" value={'Bitcoin'}>
+        <ToggleButton id="coin-radio-2" value={"Bitcoin"}>
           Bitcoin
         </ToggleButton>
       </ToggleButtonGroup>
       <br />
-     
-      <ToggleButtonGroup type="radio" name="currency" id="currency-select" defaultValue={'GBP'}
-      onChange={handleChangeCurrency}>
-        <ToggleButton id="currency-radio-1" value={'GBP'}>
+
+      <ToggleButtonGroup
+        type="radio"
+        name="currency"
+        id="currency-select"
+        defaultValue={"GBP"}
+        onChange={handleChangeCurrency}
+      >
+        <ToggleButton id="currency-radio-one" value={"GBP"}>
           GBP
         </ToggleButton>
-        <ToggleButton id="currency-radio-2" value={'USD'}>
+        <ToggleButton id="currency-radio-two" value={"USD"}>
           USD
         </ToggleButton>
       </ToggleButtonGroup>
 
-      <CoinFetch 
-      coinValue={coinValue}
-      currency={currency} />
-
-      
+      <CoinFetch coinValue={coinValue} currency={currency} />
     </>
   );
 }
